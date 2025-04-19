0fac5747754f4f1c9d6d5ec565694aeb03b6b8e4
Polina Sidorina <polishas206@gmail.com>
2025-04-19 21:43:35 +0300

    0.2

--- my-app/src/main/java/org/example/App.java
+++ my-app/src/main/java/org/example/App.java
@@ -1,13 +1,12 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+      double val=Double.parseDouble("2.0");
+      Sqrt sqrt=new Sqrt(val);
+      double result=sqrt.calc();
+      System.out.println("Sqrt of " + val + " = " + result);
     }
 }
